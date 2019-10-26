4cc2d4cd829f7717fe006c98806212469ea7ba37
cornellimartin <cor16006@byui.edu>
2019-10-25 23:06:16 -0300

    chill fix

--- lesson6/script/windchill.js
+++ lesson6/script/windchill.js
@@ -2,5 +2,5 @@ var temp = document.getElementById("temp").nodeValue;
 var windSpeed = document.getElementById("windSpeed").nodeValue;
 var chill;
 
-chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * t * Math.pow(windSpeed, 0.16));
+chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
 document.getElementById("windChill").innerHTML = chill;
