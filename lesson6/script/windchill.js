8935e0db66f3fe7382dc4e72c6372cd30aa188dd
cornellimartin <cor16006@byui.edu>
2019-10-25 23:21:43 -0300

    Final 6

--- lesson6/script/windchill.js
+++ lesson6/script/windchill.js
@@ -2,5 +2,10 @@ var temp = document.getElementById("temp").nodeValue;
 var windSpeed = document.getElementById("windSpeed").nodeValue;
 var chill;
 
-chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
+if (temp <= 50 && windSpeed > 4.8) {
+    chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
+} else {
+    chill = 0;
+}
+
 document.getElementById("windChill").innerHTML = chill;
