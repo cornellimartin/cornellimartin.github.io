7757e2c8a6bd7f4d8a078d3b95b860f42e09dad3
cornellimartin <cor16006@byui.edu>
2019-10-25 23:34:44 -0300

    Update

--- lesson6/script/windchill.js
+++ lesson6/script/windchill.js
@@ -1,9 +1,10 @@
-var temp = document.getElementById("temp").nodeValue;
-var windSpeed = document.getElementById("windSpeed").nodeValue;
+var temp = parseFloat(document.getElementById("temp").innerHTML);
+var windSpeed = parseFloat(document.getElementById("windSpeed").innerHTML);
 var chill;
 
 if (temp <= 50 && windSpeed > 4.8) {
     chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
+    chill = Math.round(chill * 100) / 100;
 } else {
     chill = "N/A";
 }
