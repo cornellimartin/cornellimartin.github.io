530917232a77ce1b764023e171445da46ebc9495
cornellimartin <cor16006@byui.edu>
2019-11-23 14:12:40 -0300

    last test

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -37,12 +37,14 @@ fetch(apiURL2)
     var dayTemp = document.getElementsByClassName("temp");
     var forecastImg = document.getElementsByClassName("forecastImg");
     var imagesrc;
-    console.log(dayTemp);
-    console.log(forecastImg);
+    console.log(dayTemp); // test days array
+    console.log(forecastImg); // test images array
     for (i = 0; i < jsObject2.list.length; i++) {
       if (jsObject2.list[i].dt_txt.includes('18:00:00')) {
-        console.log(jsObject2.list[i].main.temp);   
-        console.log(dayTemp[i]);
+        console.log(jsObject2.list[i].main.temp); // test current temp  
+        console.log(dayTemp[i]); // test current day
+        console.log(jsObject2.list[i].weather.icon); // test current icon
+        console.log(forecastImg[i]); // test current image
         dayTemp[i].textContent = jsObject2.list[i].main.temp + "º F";
         imagesrc = 'https://openweathermap.org/img/wn/' + jsObject2.list[i].weather.icon + '@2x.png';
         forecastImg[i].setAttribute ('src', imagesrc);        
