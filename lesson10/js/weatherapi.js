c3ddf5196e91d46c090721d3688443bca89bacd9
cornellimartin <cor16006@byui.edu>
2019-11-23 14:33:54 -0300

    Fixes

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -31,70 +31,24 @@ fetch(apiURL)
 
 const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?&id=5604473&units=imperial&APPID=dd66e694e0d52ba4a1b5e0c95a8a7857";
 
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 fetch(apiURL2)
   .then((response) => response.json())
   .then((jsObject2) => {
+    var counter = 0;
     var dayTemp = document.getElementsByClassName("temp");
     var forecastImg = document.getElementsByClassName("forecastImg");
+    var days = document.getElementsByClassName("days");
     var imagesrc;
-    console.log(dayTemp); // test days array
-    console.log(forecastImg); // test images array
     for (i = 0; i < jsObject2.list.length; i++) {
       if (jsObject2.list[i].dt_txt.includes('18:00:00')) {
-        console.log(jsObject2.list[i].main.temp); // test current temp  
-        console.log(dayTemp[i]); // test current day
-        console.log(jsObject2.list[i].weather.icon); // test current icon
-        console.log(forecastImg[i]); // test current image
-        dayTemp[i].textContent = jsObject2.list[i].main.temp + "º F";
-        imagesrc = 'https://openweathermap.org/img/wn/' + jsObject2.list[i].weather.icon + '@2x.png';
-        forecastImg[i].setAttribute ('src', imagesrc);        
+        dayTemp[counter].textContent = jsObject2.list[i].main.temp + "º F";
+        imagesrc = 'https://openweathermap.org/img/wn/' + jsObject2.list[i].weather[0].icon + '@2x.png';
+        forecastImg[counter].setAttribute ('src', imagesrc);     
+        var day = new Date(jsObject2.list[i].dt_txt); 
+        days[counter].textContent = daysOfWeek[day.getDay()];  
+        counter++;
       }
     }
   });
-
-
- /******************************DAYS OF THE WEEK******************************/
-  var days = document.getElementsByClassName("days");
-  
-  var today = new Date();
-  var day;
-  var i;
-
-  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  switch (today.getDay()) {
-    case 0:
-      day = "Sun";
-      i = 0;
-      break;
-    case 1:
-      day = "Mon";
-      i = 1;
-      break;
-    case 2:
-      day = "Tue";
-      i = 3;
-      break;
-    case 3:
-      day = "Wed";
-      i = 4;
-      break;
-    case 4:
-      day = "Thu";
-      i = 5;
-      break;
-      case 5:
-    day = "Fri";
-    break;
-      case 6:
-      day = "Sat";
-      i = 6;
-  }
-
-  for (x = 0; x < days.length; x++){
-    if (i > 6) {
-      i = 0;
-    }
-    days[x].textContent = daysOfWeek[i];
-    i++;
-  }
