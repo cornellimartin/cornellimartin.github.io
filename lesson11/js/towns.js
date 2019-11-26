38db46d394681f6b9451f25dd58aa0de2962fe80
cornellimartin <cor16006@byui.edu>
2019-11-26 09:22:13 -0300

    Upload first draft

---
+++ lesson11/js/towns.js
@@ -0,0 +1,69 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns = jsonObject['towns'];
+
+    for (let i = 0; i < towns.length; i++ ) {
+      if (towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven") {
+        let section = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let motto = document.createElement('h4');
+        let yearFounded = document.createElement('p');
+        let population = document.createElement('p');
+        let annualRainfall = document.createElement('p');
+        let image = document.createElement('img');
+        let imageDiv = document.createElement('div');
+        let otherDiv = document.createElement('div');
+
+        imageDiv.className = "column";
+        otherDiv.className = "column";
+    
+        h2.textContent = towns[i].name;
+        motto.textContent = towns[i].motto;
+        yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        population.textContent = 'Population: ' + towns[i].currentPopulation;
+        annualRainfall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+        image.setAttribute('src', 'images/' + towns[i].photo);
+        image.setAttribute('alt', towns[i].name);
+        
+
+        otherDiv.appendChild(h2);
+        otherDiv.appendChild(motto);
+        otherDiv.appendChild(yearFounded);
+        otherDiv.appendChild(population);
+        otherDiv.appendChild(annualRainfall);
+        imageDiv.appendChild(image);
+        section.appendChild(otherDiv);
+        section.appendChild(imageDiv);
+        
+        document.querySelector('div.towns').appendChild(section);
+    }
+    }
+  });
+
+
+/*
+Display the individual town data with the motto, year founded, population, and annual rainfall.
+
+
+"towns": [
+{
+"name": "Franklin",
+"photo": "franklin.jpg",
+"motto": "Where you will grow!",
+"yearFounded": 1788,
+"currentPopulation": 30458,
+"averageRainfall": 21,
+"events": [
+"March 4: March to the Drum of Donuts",
+"September 5 - 11: Founder Days",
+"December 1 - 26: Christmas in the Heart"
+]
+},
+
+*/
---
+++ lesson11/js/weatherapi.js
@@ -0,0 +1,54 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&id=5604473&units=imperial&APPID=dd66e694e0d52ba4a1b5e0c95a8a7857";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    document.getElementById("currentDescription").textContent = jsObject.weather[0].description;
+    document.getElementById("high").textContent = jsObject.main.temp_max + " ºF";
+    document.getElementById("humidity").textContent = jsObject.main.humidity + "%"
+    document.getElementById("windSpeed").textContent = jsObject.wind.speed + " mph";
+
+    var temp = jsObject.main.temp;
+    var windSpeed = jsObject.wind.speed;
+    var chill;
+
+    if (temp <= 50 && windSpeed > 4.8) {
+      chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
+      chill = Math.round(chill * 100) / 100;
+      chill = chill + " *F";
+    } else {
+      chill = "N/A";
+    }
+
+    document.getElementById("windChill").innerHTML = chill;
+    //const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  
+    /* document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('weather icon').setAttribute('src', imagesrc); 
+    document.getElementById('weather icon').setAttribute('alt', desc); */
+  });
+
+/******************************FORECAST FOR 5 DAYS******************************/
+
+const apiURL2 = "https://api.openweathermap.org/data/2.5/forecast?&id=5604473&units=imperial&APPID=dd66e694e0d52ba4a1b5e0c95a8a7857";
+
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+fetch(apiURL2)
+  .then((response) => response.json())
+  .then((jsObject2) => {
+    var counter = 0;
+    var dayTemp = document.getElementsByClassName("temp");
+    var forecastImg = document.getElementsByClassName("forecastImg");
+    var days = document.getElementsByClassName("days");
+    var imagesrc;
+    for (i = 0; i < jsObject2.list.length; i++) {
+      if (jsObject2.list[i].dt_txt.includes('18:00:00')) {
+        dayTemp[counter].textContent = jsObject2.list[i].main.temp + "º F";
+        imagesrc = 'https://openweathermap.org/img/wn/' + jsObject2.list[i].weather[0].icon + '@2x.png';
+        forecastImg[counter].setAttribute ('src', imagesrc);     
+        var day = new Date(jsObject2.list[i].dt_txt); 
+        days[counter].textContent = daysOfWeek[day.getDay()];  
+        counter++;
+      }
+    }
+  });
