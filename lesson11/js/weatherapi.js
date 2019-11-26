5deef286fc8175f4f9d820dc1b7f4dc92954d158
cornellimartin <cor16006@byui.edu>
2019-11-26 20:08:22 -0300

    wind chill fix

--- lesson11/js/weatherapi.js
+++ lesson11/js/weatherapi.js
@@ -12,7 +12,7 @@ fetch(apiURL)
     var windSpeed = jsObject.wind.speed;
     var chill;
 
-    if (temp <= 50 && windSpeed > 4.8) {
+    if (temp <= 50 && windSpeed > 3) {
       chill = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temp * Math.pow(windSpeed, 0.16));
       chill = Math.round(chill * 100) / 100;
       chill = chill + " *F";
