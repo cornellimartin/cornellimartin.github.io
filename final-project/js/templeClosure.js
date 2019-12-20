48b8ad19e823ef65f159d96f56d6fcd81e4e85e1
cornellimartin <cor16006@byui.edu>
2019-12-20 02:04:31 -0300

    temple completed

--- final-project/js/templeClosure.js
+++ final-project/js/templeClosure.js
@@ -1,7 +1,24 @@
-var apiURL = "../data/temples.json";
+var temples = document.getElementsByClassName("closure");
 
-fetch(apiURL)
+fetch("data/temples.json")
   .then((response) => response.json())
   .then((jsObject) => {
+    const schedules = jsObject['temples'];
     console.log(jsObject);
+    console.log(schedules);
+    console.log(schedules[0]);
+
+    for (var i = 0; i < schedules.length; i++){
+        var list = document.createElement('ul');
+        
+
+       for (let j = 0; j < schedules[i].closure.length; j++){
+            var line = document.createElement('li');
+            line.textContent = schedules[i].closure[j];
+            list.appendChild(line);
+            var selector = document.querySelectorAll("td.closure");
+            selector[i].appendChild(list);
+        }       
+    }
+
   })
